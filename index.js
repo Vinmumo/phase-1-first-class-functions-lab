4b4b5da6e6d77d6f92dcd005c54337f12c777b08
vincent <vinnymummo@gmail.com>
2024-03-25 11:15:47 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const drivers = ['Antonia','Nuru','Amari','Mo'];
+function returnFirstTwoDrivers(drivers){
+    const firstTwo = drivers.slice(0,2);
+    return firstTwo
+}
+function returnLastTwoDrivers(drivers){
+    const lastTwo = drivers.slice(-2);
+    return lastTwo
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
 // Code your solution in this file!
+function createFareMultiplier(fare){
+  return function fareQuintupler(fare){
+    let quad = (fare * 4) + fare
+    return quad
+  }
+}
+const double = createFareMultiplier()
+function fareDoubler(fare){
+   return fare * 2
+}
+function fareTripler(fare){
+    return fare * 3
+ }
+ function selectDifferentDrivers(drivers, func){
+      return func(drivers);
+ }
